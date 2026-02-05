f803252e965d09f317becb018bb413bccf709027
huyperfect <huydkde170020@fpt.edu.vn>
2026-02-05 16:45:50 +0700

    get all foods discounted

---
+++ SMAS_Repositories/FoodRepositories/FoodRepository.cs
@@ -0,0 +1,59 @@
+﻿using SMAS_BusinessObject.DTOs.Food;
+using SMAS_BusinessObject.Models;
+using SMAS_DataAccess.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAS_Repositories.FoodRepositories
+{
+    public class FoodRepository : IFoodRepository
+    {
+        private readonly FoodDAO _foodDAO;
+
+        public FoodRepository(FoodDAO foodDAO)
+        {
+            _foodDAO = foodDAO;
+        }
+
+        public async Task<IEnumerable<FoodListResponse>> GetAllFoodsCategoryAsync()
+        {
+            var foods = await _foodDAO.GetAllFoodsCategoryAsync();
+
+            return  foods.Select(f => new FoodListResponse
+            {
+                FoodId = f.FoodId,
+                Name = f.Name,
+                Description = f.Description,
+                Price = f.Price,
+                PromotionalPrice = f.PromotionalPrice,
+                Image = f.Image,
+                Unit = f.Unit,  
+                IsAvailable = f.IsAvailable,
+                IsDirectSale = f.IsDirectSale,
+                IsFeatured = f.IsFeatured,
+                PreparationTime = f.PreparationTime,
+                Calories = f.Calories,
+                ViewCount = f.ViewCount,
+                OrderCount = f.OrderCount,
+                Rating = f.Rating,
+                Note = f.Note,
+                CreatedAt = f.CreatedAt,
+                UpdatedAt = f.UpdatedAt,
+                Categories = f.Categories.Select(c => new CategoryFoodListResponse
+                {
+                    CategoryId = c.CategoryId,
+                    Name = c.Name,
+                    Description = c.Description,
+                    IsProcessedGoods = c.IsProcessedGoods,
+                    Image = c.Image,
+                    IsAvailable = c.IsAvailable,
+                    CreatedAt = c.CreatedAt,
+                    UpdatedAt = c.UpdatedAt
+                }).ToList()
+            });
+        }
+    }
+}
---
+++ SMAS_Services/FoodServices/FoodService.cs
@@ -0,0 +1,34 @@
+﻿using SMAS_BusinessObject.DTOs.Food;
+using SMAS_Repositories.FoodRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAS_Services.FoodServices
+{
+    public class FoodService : IFoodService
+    {
+
+        private readonly IFoodRepository _foodRepository;
+
+        public FoodService(IFoodRepository foodRepository)
+        {
+            _foodRepository = foodRepository;
+        }
+
+        public async Task<IEnumerable<FoodListResponse>> GetAllFoodsCategoryAsync()
+        {
+            return await _foodRepository.GetAllFoodsCategoryAsync();
+        }
+
+        public async Task<IEnumerable<FoodListResponse>> GetAllFoodsDiscountAsync()
+        {
+            var foods = await _foodRepository.GetAllFoodsCategoryAsync();
+
+            return foods.Where(f => f.PromotionalPrice.HasValue && 
+                                                   f.PromotionalPrice.Value < f.Price);
+        }
+    }
+}
