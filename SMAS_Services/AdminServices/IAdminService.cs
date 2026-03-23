2524560dd4aebcc2de8f7824c3e448120f7187b7
nahinchu <hoangnahinchu5504@gmail.com>
2026-03-23 12:44:35 +0700

    admin dashboard

---
+++ SMAS_Services/AdminServices/IAdminService.cs
@@ -0,0 +1,17 @@
+﻿using SMAS_BusinessObject.DTOs.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAS_Services.AdminServices
+{
+    public interface IAdminService
+    {
+        Task<DashboardSummaryDto> GetSummaryAsync(int? month, int? year);
+        Task<RevenueChartDto> GetRevenueChartAsync(int? months, int? year);
+        Task<OrderStructureDto> GetOrderStructureAsync(int? month, int? year);
+        Task<List<WarehouseTransactionDto>> GetWarehouseTransactionsAsync(int? limit);
+    }
+}
